e1f81cbeb08b356241a854380547a458b20f8d0d
G Prabhakar <prabhakar2095@gmail.com>
2021-02-20 15:32:41 +0530

    added banner

---
+++ Components/Banner.js
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+const Banner = () => (
+    <div className='banner'>
+        <h1>Hello, I'm Prabhakar</h1>
+        <h2>are you looking for a web developer?</h2>
+        <button>View my work</button>
+    </div>
+)
+
+export default Banner
---
+++ src/Components/banner/Banner.js
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+import './banner.css'
+import profile from './../../images/profile.png'
+
+
+const Banner = () => (
+    <div className='banner Home'>
+        <div id='intro'>
+            <img src={profile} alt='profile'/>
+            <h1>Hello, I'm <span>Prabhakar</span></h1>
+            <h2>are you looking for a web developer?</h2>
+            <button><a href='#'>View my work</a></button>
+        </div>
+    </div>
+)
+
+export default Banner
