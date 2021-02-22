91c13ced9bd7023810ab2a966bc09eaef134e95e
G Prabhakar <prabhakar2095@gmail.com>
2021-02-22 10:10:13 +0530

    started adding projects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import './App.css';
 import Banner from './Components/banner/Banner'
+import Projects from './Components/projects/Projects'
 import Contact from './Components/contact/Contact'
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <Banner />
         <Contact />
+        <Projects />
       </div>
     )
   }
---
+++ src/Components/projects/Projects.js
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+class Projects extends Component {
+    render() {
+        return (
+            <div>hello world</div>
+        )
+    }
+}
+
+export default Projects
