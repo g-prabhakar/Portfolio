070c80407a55f06c576099df18809dfa3139343f
G Prabhakar <prabhakar2095@gmail.com>
2021-02-23 16:24:38 +0530

    banner complete

--- src/Components/banner/Banner.js
+++ src/Components/banner/Banner.js
@@ -3,8 +3,7 @@ import profile from '../../images/profile.png'
 
 
 const Banner = () => (
-    <div>
-       <div className='banner Home' id='banner'>
+       <div className='banner' id='banner'>
             <div id='intro'>
                 <img src={profile} alt='profile'/>
                 <h1>Hello, I'm <span>Prabhakar</span></h1>
@@ -12,7 +11,6 @@ const Banner = () => (
                 <button id='work'><a href='#projects'>View my work <i class="fa fa-arrow-down"></i></a></button>
             </div>
         </div>
-    </div>
 )
 
 export default Banner
